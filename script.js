8d7d5a399f5442be9cab30cc0c8f6447a5b9b6c2
garrettrogers002 <garrett.rogers002@gmail.com>
2025-05-25 00:32:05 -0400

    allow to change grid size

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cont = document.querySelector("#container");
 
 let gridSize = 16;
 let boxSize = 900 / gridSize;
+let newBtn = document.querySelector("#newBtn");
 console.log("beep");
 
 function loadthing () {
@@ -17,10 +18,22 @@ function loadthing () {
         cont.appendChild(row);
     }
 }
+function changeGridSize (input) {
+    cont.innerHTML = "";
+    gridSize = input;
+    boxSize = 900 / gridSize;
+    loadthing();
+}
 document.addEventListener("DOMContentLoaded", function () {
     loadthing();
     console.log("boop");
 })
 cont.addEventListener("mouseover", function (e) {
     e.target.style.backgroundColor = "blue";
+    addButton();
+})
+newBtn.addEventListener("mousedown", function () {
+    let newGridSize = prompt("input new grid size: ", "25");
+    let newGridSizee = parseInt(newGridSize);
+    changeGridSize(newGridSizee);
 })
